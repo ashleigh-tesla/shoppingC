2fdc8eb9d93d357a53677d03eb583a4ab3e0e4c3
ashleigh-tesla <84648968+ashleigh-tesla@users.noreply.github.com>
2021-11-24 13:27:11 +0200

    updated index.html then added main.js

---
+++ main.js
@@ -0,0 +1,85 @@
+let carts = document.querySelectorAll('.add-cart')
+
+let products = [{
+        name: 'Grey Tshirt',
+        tag: 'greytshirt',
+        price: 15,
+        inCart: 0
+    },
+    {
+        name: 'Grey Hoodie',
+        tag: 'greythoodie',
+        price: 20,
+        inCart: 0
+    },
+    {
+        name: 'Black Tshirt',
+        tag: 'blacktshirt',
+        price: 15,
+        inCart: 0
+    },
+    {
+        name: 'Black Hoodie',
+        tag: 'blackhoodie',
+        price: 20,
+        inCart: 0
+    }
+]
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartNumbers(products[i])
+    })
+}
+
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers')
+
+    if (productNumbers) {
+        document.querySelector('.cart span').textContent = productNumbers
+    }
+}
+
+function cartNumbers(product) {
+    let productNumbers = localStorage.getItem('cartNumbers')
+    console.log(typeof productNumbers)
+
+    productNumbers = parseInt(productNumbers)
+    console.log(typeof productNumbers)
+
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1)
+        document.querySelector('.cart span').textContent = productNumbers + 1
+    } else {
+        localStorage.setItem('cartNumbers', 1)
+        document.querySelector('.cart span').textContent = 1
+    }
+    setItems(product)
+}
+
+function setItems(product) {
+
+    let cartItems = localStorage.getItem('productsInCart')
+    console.log("Inside Of SetItems Function")
+    console.log("My Product Is", product)
+
+    cartItems = JSON.parse(cartItems)
+    console.log("My cartItems are", cartItems)
+
+    if (cartItems != null) {
+
+        if (cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1
+    } else {
+        product.inCart = 1
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems))
+}
+onLoadCartNumbers()
