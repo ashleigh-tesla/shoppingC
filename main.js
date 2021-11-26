d13ff39d615d6fbd6c776ab7cea6cb292d9a697f
ashleigh-tesla <84648968+ashleigh-tesla@users.noreply.github.com>
2021-11-26 06:58:04 +0200

    updated cart.html ; index.html ; main.js & style.css also updated images

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let products = [{
     },
     {
         name: 'Grey Hoodie',
-        tag: 'greythoodie',
+        tag: 'greyhoodie',
         price: 20,
         inCart: 0
     },
@@ -98,4 +98,45 @@ function totalCost(product) {
         localStorage.setItem("totalCost", product.price)
     }
 }
+
+function displayCart() {
+    let cartItems = localStorage.getItem('productsInCart')
+    cartItems = JSON.parse(cartItems)
+
+    console.log(cartItems)
+
+    let productContainer = document.querySelector(".products")
+    let cartCost = localStorage.getItem('totalCost')
+
+    if (cartItems && productContainer) {
+        productContainer.innerHTML = ''
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML = `
+            <div class="product">
+                <ion-icon name="close-circle"></ion-icon>
+                <img src="images/${item.tag}.jpg">
+                <span>${item.name}</span>
+            </div>
+            <div class="price">$${item.price},00</div>
+                <ion-icon name="arrow-back-circle-outline"></ion-icon>
+                <span>${item.inCart}</span>
+                <ion-icon name="arrow-forward-circle-outline"></ion-icon>
+            </div>
+            <div class="total">
+                $${item.inCart * item.price},00
+            </div>
+            `
+        })
+        productContainer.innerHTML += `
+        <div class="basketTotalContainer">
+            <h4 class="basketTotalTitle">
+                Basket Total
+            </h4>
+            <h4 class="basketTotal">
+            $${cartCost},00
+            </h4>
+            `
+    }
+}
 onLoadCartNumbers()
+displayCart()
